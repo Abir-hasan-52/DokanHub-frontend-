c8c10e8be904c7e34c3e678ea2542a74226e8cea
Abir-Hasan-52 <abirhasan5208+github@gmail.com>
2025-11-17 22:58:16 +0600

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "./index.css";
+import Navbar from "./Pages/Shared/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/Shared/Logo/Logo.jsx
@@ -0,0 +1,27 @@
+import { motion } from "framer-motion";
+
+export default function Logo() {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: -6 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.4 }}
+      className="flex items-center gap-2 select-none cursor-pointer"
+    >
+      {/* Icon */}
+      <motion.div
+        initial={{ rotate: -8, scale: 0.9 }}
+        animate={{ rotate: 0, scale: 1 }}
+        transition={{ duration: 0.4 }}
+        className="w-10 h-10 bg-orange-500 flex items-center justify-center rounded-xl shadow-md"
+      >
+        <span className="text-white text-2xl font-bold">D</span>
+      </motion.div>
+
+      {/* Text */}
+      <h1 className="text-2xl font-bold text-gray-800 tracking-wide">
+        Dokan<span className="text-orange-500">Hub</span>
+      </h1>
+    </motion.div>
+  );
+}
